6cedb77d9f413f7ed3d2f3f240abb1f8ee348013
veerookorrapati <veerookorrapati@veeroo.korrapati-air-1020>
2020-11-06 18:31:36 -0600

    Created Vehicle Class

---
+++ Mitchell_Project/src/Vehicle.java
@@ -0,0 +1,41 @@
+
+public class Vehicle {
+	private int Id;
+	private int Year;
+	private String Make;
+	private String Model;
+	
+	//Id-------------
+	public int getId() {
+		return Id;
+	}
+	public void setId(int Id) {
+		this.Id = Id;
+	}
+	
+	//Year------------
+	public int getYear() {
+		return Year;
+	}
+	public void setYear(int Year) {
+		this.Year = Year;
+	}
+	
+	//Make------------
+	public String getMake() {
+		return Make;
+	}
+	public void setMake(String Make) {
+		this.Make = Make;
+	}
+	
+	//Model-------------
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String Model) {
+		this.Model = Model;
+	}
+	
+	
+}
